6f3cf71cd9eba5125eac4f5f653d45abe128b9b8
ramongallego <ramongallego@gmail.com>
2018-01-08 16:02:07 -0800

    Now it works, generates 4 fastqs per sample

--- scripts/functions/strip_path.sh
+++ scripts/functions/strip_path.sh
@@ -4,30 +4,10 @@
 # Usage source strip_path.sh file varname
 # returns short_file as the object with the new name
 
-echo $1
-echo $2
+#echo $1
+#echo $2
 suffix=$2
 
 path_to_delete="$(dirname "${1}" | sed 's_/_\\/_g')"
 declare short_$suffix=$(echo "$1" | sed "s/$path_to_delete//g")
 varname=short_$suffix
-
-
-
-
-#varname= short_$2
-
-#declare varname="try"
-
-#echo "${!varname}"
-#declare short_$2=$(echo "$1" | sed "s/$path_to_delete//g")
-#varname= short_$1
-#echo "${!varname}"
-#echo "${path_to_delete}"
-
-#newpath=$(echo "$path_to_delete" | sed 's_/_\\/_g')
-
-#echo "$1" | sed "s/$path_to_delete//g"
-#suffix=bzz
-
-#...and then...
