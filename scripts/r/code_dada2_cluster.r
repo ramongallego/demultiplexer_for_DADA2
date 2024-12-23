7573fd8eec33455ab0f1df2ee0871c6f179413c1
ramongallego <ramongallego@gmail.com>
2024-12-23 23:04:51 +0100

    dropna

--- scripts/r/code_dada2_cluster.r
+++ scripts/r/code_dada2_cluster.r
@@ -42,7 +42,7 @@ files.noprimers |>
 # Keep only real filenames and complete cases
 
 files.noprimers |>
- filter(if_all(any_of(c(Fwd.R1 ,  Fwd.R2, Rev.R1, Rev.R2)), ~ !is.na(.x))) |>
+ drop_na() |>
  inner_join(sample.map |> 
  select(fastq_header, Sample )) -> files.noprimers
 
