b6d126ac5bd3ebc0ade167274e90bb09f695ab25
ramongallego <ramongallego@gmail.com>
2025-03-10 12:39:56 +0100

    d

--- scripts/vsearch_clustering.sh
+++ scripts/vsearch_clustering.sh
@@ -11,43 +11,47 @@ HASH=$3
 LENR1=$4
 LENR2=$5
 
-# We should reverse the _Rev files and concatenate them after the Fwd ones
-for file in "${NOPRIMERS_DIR}"/*_Fwd.R*.fastq; do
+for file in "${NOPRIMERS_DIR}"/*R1.fastq; do
 
-fwd_file=$(basename $file)
-rev_file=$(echo $fwd_file | sed -E 's/_Fwd\.R([12]\.fastq)$/_Rev.R\1/')
+    R1_file=$(basename $file)
+    R2_file=$(echo $R1_file | sed 's/R1.fastq$/R2.fastq/')
 
-echo "${NOPRIMERS_DIR}"/$fwd_file
-echo "${NOPRIMERS_DIR}"/$rev_file
+    merged_file=$(echo $R1_file | sed 's/R1.fastq$/merged.fasta/')
+    unmerged_file=$(echo $R1_file | sed 's/R1.fastq$/un_merged.fasta/')
+    
+
+    ## TRIM to length with cutadapt, remove Ns
+
+
+    cutadapt -j 0 \
+        -u 0 -U 0  \
+        -l "${LENR1}" -L "${LENR2}" --max-n 0 \
+        -o "${OUTPUT_FOLDER}"/"${R1_file}" -p "${OUTPUT_FOLDER}"/"${R2_file}" \
+        "${NOPRIMERS_DIR}"/$R1_file "${NOPRIMERS_DIR}"/$R2_file
+
+
+    vsearch --fastq_mergepairs "${OUTPUT_FOLDER}"/$R1_file --reverse "${OUTPUT_FOLDER}"/$R2_file --fastaout "${OUTPUT_FOLDER}"/"${merged_file}" \
+        --fastaout_notmerged_fwd "${OUTPUT_FOLDER}"/"${unmerged_file}"
 
 done
 
-# for file in "${NOPRIMERS_DIR}"/*R1.fastq; do
-# fwd_file=$(basename $file)
-# rev_file=$(echo $fwd_file | sed 's/R1.fastq$/R2.fastq/')
+for file in "${OUTPUT_FOLDER}"/*_Fwd.merged.fasta$; do
 
-# merged_file=$(echo $fwd_file | sed 's/R1.fastq$/merged.fasta/')
-# unmerged_file=$(echo $fwd_file | sed 's/R1.fastq$/un_merged.fasta/')
-# derep_file=$(echo $fwd_file | sed 's/R1.fastq$/derep.fasta/')
+    # We should reverse the _Rev files and concatenate them after the Fwd ones, but do that after merging R1 and R2
 
-# centroids_file=$(echo $fwd_file | sed 's/R1.fastq$/centroids.fasta/')
+    fwd_file=$(basename $file)
+    rev_file=$(echo $fwd_file | sed 's/_Fwd.merged.fasta$/_Rev.merged.fasta/')
 
-# ## TRIM to length with cutadapt, remove Ns
+    derep_file=$(echo $fwd_file | sed 's/_Fwd.merged.fasta$/_derep.fasta/')
 
+    centroids_file=$(echo $fwd_file | sed 's/_Fwd.merged.fasta$/_centroids.fasta/')
 
-# cutadapt -j 0 \
-#  -u 0 -U 0  \
-#  -l "${LENR1}" -L "${LENR2}" --max-n 0 \
-#  -o "${OUTPUT_FOLDER}"/"${fwd_file}" -p "${OUTPUT_FOLDER}"/"${rev_file}" \
-#  "${NOPRIMERS_DIR}"/$fwd_file "${NOPRIMERS_DIR}"/$rev_file
+    revcom "${OUTPUT_FOLDER}"/$rev_file >> "${OUTPUT_FOLDER}"/$fwd_file
+
+    vsearch --fastx_uniques "${OUTPUT_FOLDER}"/"${merged_file}" --sizeout --fastaout "${OUTPUT_FOLDER}"/"${derep_file}"
+
+    vsearch --cluster_unoise "${OUTPUT_FOLDER}"/"${derep_file}" --centroids "${OUTPUT_FOLDER}"/"${centroids_file}" --sizein --sizeout --minsize 1
 
 
-# vsearch --fastq_mergepairs "${OUTPUT_FOLDER}"/$fwd_file --reverse "${OUTPUT_FOLDER}"/$rev_file --fastaout "${OUTPUT_FOLDER}"/"${merged_file}" --fastaout_notmerged_fwd "${OUTPUT_FOLDER}"/"${unmerged_file}"
 
-# vsearch --fastx_uniques "${OUTPUT_FOLDER}"/"${merged_file}" --sizeout --fastaout "${OUTPUT_FOLDER}"/"${derep_file}"
-
-# vsearch --cluster_unoise "${OUTPUT_FOLDER}"/"${derep_file}" --centroids "${OUTPUT_FOLDER}"/"${centroids_file}" --sizein --sizeout --minsize 1 
-
-# done
-
-# ## Now we should 
+done
