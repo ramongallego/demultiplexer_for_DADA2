52e3731a7358ef86a044c085261b671a031f876f
ramongallego <ramongallego@gmail.com>
2025-03-10 11:51:43 +0100

    d

--- scripts/vsearch_clustering.sh
+++ scripts/vsearch_clustering.sh
@@ -22,32 +22,32 @@ echo $rev_file
 head $rev_file
 done
 
-for file in "${NOPRIMERS_DIR}"/*R1.fastq; do
-fwd_file=$(basename $file)
-rev_file=$(echo $fwd_file | sed 's/R1.fastq$/R2.fastq/')
+# for file in "${NOPRIMERS_DIR}"/*R1.fastq; do
+# fwd_file=$(basename $file)
+# rev_file=$(echo $fwd_file | sed 's/R1.fastq$/R2.fastq/')
 
-merged_file=$(echo $fwd_file | sed 's/R1.fastq$/merged.fasta/')
-unmerged_file=$(echo $fwd_file | sed 's/R1.fastq$/un_merged.fasta/')
-derep_file=$(echo $fwd_file | sed 's/R1.fastq$/derep.fasta/')
+# merged_file=$(echo $fwd_file | sed 's/R1.fastq$/merged.fasta/')
+# unmerged_file=$(echo $fwd_file | sed 's/R1.fastq$/un_merged.fasta/')
+# derep_file=$(echo $fwd_file | sed 's/R1.fastq$/derep.fasta/')
 
-centroids_file=$(echo $fwd_file | sed 's/R1.fastq$/centroids.fasta/')
+# centroids_file=$(echo $fwd_file | sed 's/R1.fastq$/centroids.fasta/')
 
-## TRIM to length with cutadapt, remove Ns
-
-
-cutadapt -j 0 \
- -u 0 -U 0  \
- -l "${LENR1}" -L "${LENR2}" --max-n 0 \
- -o "${OUTPUT_FOLDER}"/"${fwd_file}" -p "${OUTPUT_FOLDER}"/"${rev_file}" \
- "${NOPRIMERS_DIR}"/$fwd_file "${NOPRIMERS_DIR}"/$rev_file
+# ## TRIM to length with cutadapt, remove Ns
 
 
-vsearch --fastq_mergepairs "${OUTPUT_FOLDER}"/$fwd_file --reverse "${OUTPUT_FOLDER}"/$rev_file --fastaout "${OUTPUT_FOLDER}"/"${merged_file}" --fastaout_notmerged_fwd "${OUTPUT_FOLDER}"/"${unmerged_file}"
+# cutadapt -j 0 \
+#  -u 0 -U 0  \
+#  -l "${LENR1}" -L "${LENR2}" --max-n 0 \
+#  -o "${OUTPUT_FOLDER}"/"${fwd_file}" -p "${OUTPUT_FOLDER}"/"${rev_file}" \
+#  "${NOPRIMERS_DIR}"/$fwd_file "${NOPRIMERS_DIR}"/$rev_file
 
-vsearch --fastx_uniques "${OUTPUT_FOLDER}"/"${merged_file}" --sizeout --fastaout "${OUTPUT_FOLDER}"/"${derep_file}"
 
-vsearch --cluster_unoise "${OUTPUT_FOLDER}"/"${derep_file}" --centroids "${OUTPUT_FOLDER}"/"${centroids_file}" --sizein --sizeout --minsize 1 
+# vsearch --fastq_mergepairs "${OUTPUT_FOLDER}"/$fwd_file --reverse "${OUTPUT_FOLDER}"/$rev_file --fastaout "${OUTPUT_FOLDER}"/"${merged_file}" --fastaout_notmerged_fwd "${OUTPUT_FOLDER}"/"${unmerged_file}"
 
-done
+# vsearch --fastx_uniques "${OUTPUT_FOLDER}"/"${merged_file}" --sizeout --fastaout "${OUTPUT_FOLDER}"/"${derep_file}"
 
-## Now we should 
+# vsearch --cluster_unoise "${OUTPUT_FOLDER}"/"${derep_file}" --centroids "${OUTPUT_FOLDER}"/"${centroids_file}" --sizein --sizeout --minsize 1 
+
+# done
+
+# ## Now we should 
