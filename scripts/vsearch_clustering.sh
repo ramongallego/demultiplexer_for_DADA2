8de022ac69f77006a4bd65be3ca06b5cb086e584
ramongallego <ramongallego@gmail.com>
2025-03-10 11:55:35 +0100

    d

--- scripts/vsearch_clustering.sh
+++ scripts/vsearch_clustering.sh
@@ -15,7 +15,7 @@ LENR2=$5
 for file in "${NOPRIMERS_DIR}"/*_Fwd.R*.fastq; do
 
 fwd_file=$(basename $file)
-rev_file=$(echo $fwd_file | sed sed -E 's/(Fwd)_(R[12])/Rev_\2/')
+rev_file=$(echo $fwd_file | sed -E 's/(Fwd)_(R[12])/Rev_\2/')
 
 head "${NOPRIMERS_DIR}"/$fwd_file
 echo "${NOPRIMERS_DIR}"/$rev_file
