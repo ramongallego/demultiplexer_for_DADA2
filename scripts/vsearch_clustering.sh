437ac0467ce5c296fc1c885462c1142cf0795d4a
ramongallego <ramongallego@gmail.com>
2025-03-11 12:49:03 +0100

    d

--- scripts/vsearch_clustering.sh
+++ scripts/vsearch_clustering.sh
@@ -3,8 +3,8 @@
 ## usage bash vsearch_clustering.sh <path_to_output_folder> <path_to_no_primers_folder> <USE_HASH> <LENR1> <LENR2>
 
 MAIN_DIR="$(dirname "$0")"
-SCRIPT_DIR="${MAIN_DIR}"/scripts
-for file in "${SCRIPT_DIR}"/* ; do
+
+for file in "${MAIN_DIR}"/* ; do
 	source "${file}"
 done
 
@@ -16,6 +16,9 @@ HASH=$3
 LENR1=$4
 LENR2=$5
 
+OUTPUT_SUMMARY="${OUTPUT_FOLDER}"/vsearch_summary.csv
+echo "Sample, step, nreads" > "${OUTPUT_SUMMARY}"
+
 for file in "${NOPRIMERS_DIR}"/*R1.fastq; do
 
     R1_file=$(basename $file)
@@ -23,6 +26,7 @@ for file in "${NOPRIMERS_DIR}"/*R1.fastq; do
 
     merged_file=$(echo $R1_file | sed 's/R1.fastq$/merged.fasta/')
     unmerged_file=$(echo $R1_file | sed 's/R1.fastq$/un_merged.fasta/')
+    sample=$(echo $R1_file | sed 's/R1.fastq$//')
     
 
     ## TRIM to length with cutadapt, remove Ns
@@ -32,11 +36,17 @@ for file in "${NOPRIMERS_DIR}"/*R1.fastq; do
         -u 0 -U 0  \
         -l "${LENR1}" -L "${LENR2}" --max-n 0 \
         -o "${OUTPUT_FOLDER}"/"${R1_file}" -p "${OUTPUT_FOLDER}"/"${R2_file}" \
-        "${NOPRIMERS_DIR}"/$R1_file "${NOPRIMERS_DIR}"/$R2_file
+        "${NOPRIMERS_DIR}"/$R1_file "${NOPRIMERS_DIR}"/$R2_file  > "${OUTPUT_FOLDER}"/cutadapt_logqcontrol.txt
+
+    num=$(grep "Pairs written (passing filters)" "${OUTPUT_FOLDER}"/cutadapt_logqcontrol.txt | awk '{print $4}' | tr -d ',')
+    
+    echo "${sample}, filtering, ${num}" >> "${OUTPUT_SUMMARY}"
 
 
-    vsearch --fastq_mergepairs "${OUTPUT_FOLDER}"/$R1_file --reverse "${OUTPUT_FOLDER}"/$R2_file --fastaout "${OUTPUT_FOLDER}"/"${merged_file}" \
-        --fastaout_notmerged_fwd "${OUTPUT_FOLDER}"/"${unmerged_file}"
+    num=$(vsearch --fastq_mergepairs "${OUTPUT_FOLDER}"/$R1_file --reverse "${OUTPUT_FOLDER}"/$R2_file --fastaout "${OUTPUT_FOLDER}"/"${merged_file}" \
+        --fastaout_notmerged_fwd "${OUTPUT_FOLDER}"/"${unmerged_file}" | grep -m1 " Merged (" | awk '{print $1}')
+
+     echo "${sample}, merging, ${num}" >> "${OUTPUT_SUMMARY}"   
 
 done
 
@@ -52,6 +62,8 @@ for file in "${OUTPUT_FOLDER}"/*_Fwd.merged.fasta; do
     centroids_file=$(echo $fwd_file | sed 's/_Fwd.merged.fasta$/_centroids.fasta/')
 
     non_chimeras_file=$(echo $fwd_file | sed 's/_Fwd.merged.fasta$/_non_chimeras.fasta/')
+
+    sample=$(echo $fwd_file | sed 's/_Fwd.merged.fasta$//')
 
 
     revcom "${OUTPUT_FOLDER}"/$rev_file >> "${OUTPUT_FOLDER}"/$fwd_file
