28bff4200e4fd551963ccccdb7e2d2c49c4f27ca
ramongallego <ramongallego@gmail.com>
2025-03-17 12:23:53 +0100

    now with seq

--- scripts/Parse_Abundances.R
+++ scripts/Parse_Abundances.R
@@ -36,10 +36,10 @@ if ( grepl ("yes", params$hash, ignore.case = TRUE)) {
     ungroup() |> 
     distinct(seq) |> 
     rowwise() |>
-    mutate (Hash = sha1(seq)) -> Hash_key
+    mutate (Hash = sha1(seq)) |>
+    rename(sequence=seq) -> Hash_key
   
-  write_csv(Hash_key |>
-              rename(sequence=seq), file = file.path(params$folder,"Hash_key.csv"))
+  write_csv(Hash_key , file = file.path(params$folder,"Hash_key.csv"))
   
   eDNAfuns::fasta_writer(df = Hash_key,
                          sequence = sequence, 
@@ -47,7 +47,7 @@ if ( grepl ("yes", params$hash, ignore.case = TRUE)) {
                          file.out = file.path(params$folder, "Hash_key.fasta"))
   
   samples |> 
-    inner_join(Hash_key) |> 
+    semi_join(Hash_key) |> 
     select (Sample, locus, Hash, nReads) -> Abundance
   
   write_csv(Abundance, file = file.path(params$folder,"ASV_table.csv"))
