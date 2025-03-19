046c6effe8b3dc8872b63eb218c4cc940275b331
ramongallego <ramongallego@gmail.com>
2025-03-19 11:25:06 +0100

    Flash2

--- scripts/vsearch_clustering.sh
+++ scripts/vsearch_clustering.sh
@@ -15,7 +15,8 @@ LENR2=$5
 OUTPUT_SUMMARY="${OUTPUT_FOLDER}"/vsearch_summary.csv
 echo "Sample, step, nreads" > "${OUTPUT_SUMMARY}"
 MERGING_SUMMARY="${OUTPUT_FOLDER}"/merge_summary.csv
-echo "Sample,Pairs,Merged,Unmerged,kmers,multiple,lowscore,tooshort,staggered"  > "${MERGING_SUMMARY}"
+# echo "Sample,Pairs,Merged,Unmerged,kmers,multiple,lowscore,tooshort,staggered"  > "${MERGING_SUMMARY}"
+echo "Sample,Pairs,Merged,Unmerged"  > "${MERGING_SUMMARY}"
 
 MIDFILES="${OUTPUT_FOLDER}"/midfiles
 mkdir "${MIDFILES}"
@@ -26,7 +27,7 @@ for file in "${NOPRIMERS_DIR}"/*R1.fastq; do
     R2_file="${R1_file/R1.fastq/R2.fastq}"
     sample="${R1_file/.R1.fastq/}"
 
-    merged_file="${sample}.merged.fasta"
+    merged_file="${sample}.merged.fastq"
     unmerged_file="${sample}.un_merged.fasta"
     cutadapt_log="${MIDFILES}/${sample}_cutadapt_log.txt"
     merge_log="${MIDFILES}/${sample}_merge.log" 
@@ -48,38 +49,47 @@ for file in "${NOPRIMERS_DIR}"/*R1.fastq; do
     echo "${sample}, filtering, ${num}" >> "${OUTPUT_SUMMARY}"
     
     ## merge pairs and stats
+    ## vsearch is terrible at merging super long overlaps, we are using flash2 instead
 
-    
+    flash2 "${MIDFILES}"/$R1_file "${MIDFILES}"/$R2_file -c > "${MIDFILES}"/"${merged_file}" 2> "${merge_log}"
 
-   vsearch --fastq_mergepairs "${MIDFILES}"/$R1_file --reverse "${MIDFILES}"/$R2_file --fastaout "${MIDFILES}"/"${merged_file}" \
-    --fastq_maxdiffs 2 \
-    --fastaout_notmerged_fwd "${MIDFILES}"/"${unmerged_file}" 2> "${merge_log}"
-    
-    #Extract key stats
-        pairs=$(grep -oP '^\d+(?=\s+Pairs)' "$merge_log" || echo 0)
-        merged=$(grep -oP '^\d+(?=\s+Merged)' "$merge_log" || echo 0)
-        unmerged=$(grep -oP '^\d+(?=\s+Not merged)' "$merge_log" || echo 0)
+#    vsearch --fastq_mergepairs "${MIDFILES}"/$R1_file --reverse "${MIDFILES}"/$R2_file --fastaout "${MIDFILES}"/"${merged_file}" \
+#     --fastq_maxdiffs 2 \
+#     --fastaout_notmerged_fwd "${MIDFILES}"/"${unmerged_file}" 2> "${merge_log}"
 
-    # Extract failure reasons, setting defaults if missing
-        kmers=$(grep -oP '^\d+(?=\s+too few kmers)' "$merge_log" || echo 0)
-        multiple=$(grep -oP '^\d+(?=\s+multiple potential alignments)' "$merge_log" || echo 0)
-        lowscore=$(grep -oP '^\d+(?=\s+alignment score too low)' "$merge_log" || echo 0)
-        tooshort=$(grep -oP '^\d+(?=\s+overlap too short)' "$merge_log" || echo 0)
-        staggered=$(grep -oP '^\d+(?=\s+staggered read pairs)' "$merge_log" || echo 0)
+        pairs=$(grep "Total pairs:" "${merge_log}" | awk '{print $NF}')
+        merged=$(grep "Combined pairs:" "${merge_log}" | awk '{print $NF}')
+        unmerged=$(grep "Uncombined pairs:" "${merge_log}" | awk '{print $NF}')
+            
+    # #Extract key stats
+    #     pairs=$(grep -oP '^\d+(?=\s+Pairs)' "$merge_log" || echo 0)
+    #     merged=$(grep -oP '^\d+(?=\s+Merged)' "$merge_log" || echo 0)
+    #     unmerged=$(grep -oP '^\d+(?=\s+Not merged)' "$merge_log" || echo 0)
+
+    # # Extract failure reasons, setting defaults if missing
+    #     kmers=$(grep -oP '^\d+(?=\s+too few kmers)' "$merge_log" || echo 0)
+    #     multiple=$(grep -oP '^\d+(?=\s+multiple potential alignments)' "$merge_log" || echo 0)
+    #     lowscore=$(grep -oP '^\d+(?=\s+alignment score too low)' "$merge_log" || echo 0)
+    #     tooshort=$(grep -oP '^\d+(?=\s+overlap too short)' "$merge_log" || echo 0)
+    #     staggered=$(grep -oP '^\d+(?=\s+staggered read pairs)' "$merge_log" || echo 0)
 
     echo "${sample}, merging, ${merged}" >> "${OUTPUT_SUMMARY}"  
 
     # Append to merge summary file
-    echo "${sample},${pairs},${merged},${unmerged},${kmers},${multiple},${lowscore},${tooshort},${staggered}" >> "${MERGING_SUMMARY}"
-    
+    # echo "${sample},${pairs},${merged},${unmerged},${kmers},${multiple},${lowscore},${tooshort},${staggered}" >> "${MERGING_SUMMARY}"
+    echo "${sample},${pairs},${merged},${unmerged}" >> "${MERGING_SUMMARY}"
 done
 
-for file in "${MIDFILES}"/*_Fwd.merged.fasta; do
+# for file in "${MIDFILES}"/*_Fwd.merged.fasta; do
+for file in "${MIDFILES}"/*_Fwd.merged.fastq; do
+
 
     # We should reverse the _Rev files and concatenate them after the Fwd ones, but do that after merging R1 and R2
 
     fwd_file=$(basename "$file")
-    sample="${fwd_file/_Fwd.merged.fasta/}"
+    # sample="${fwd_file/_Fwd.merged.fasta/}"
+        sample="${fwd_file/_Fwd.merged.fastq/}"
+
 
     rev_file="${sample}_Rev.merged.fasta"
     derep_file="${sample}_derep.fasta"
